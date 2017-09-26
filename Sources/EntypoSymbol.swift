cfd5ced922346b3a9642b50a4eab3bcfbfd320c2
takuya ichise <ichise@pepabo.com>
2017-09-26 17:38:54 +0900

    Swift4モードで@objc推論が廃止されたので、ObjectiveCから呼び出してる変数・メソッドに@objcを追加した

--- Sources/EntypoSymbol.swift
+++ Sources/EntypoSymbol.swift
@@ -13,7 +13,7 @@ public class EntypoSymbol:NSObject {
     
     var mutableTextFontAttributes = [NSAttributedStringKey : Any]()
     
-    public init(text:NSString, size:CGFloat) {
+    @objc public init(text:NSString, size:CGFloat) {
         self.text = text
         
         self.mutableTextFontAttributes = [NSAttributedStringKey : Any]()
@@ -23,7 +23,7 @@ public class EntypoSymbol:NSObject {
     }
     
     // MARK: - Method
-    public func addAttribute(attributeName:NSAttributedStringKey, value:AnyObject) {
+    @objc public func addAttribute(attributeName:NSAttributedStringKey, value:AnyObject) {
         self.mutableTextFontAttributes[attributeName] = value
     }
     
@@ -32,7 +32,7 @@ public class EntypoSymbol:NSObject {
      - parameter size: サイズ
      - returns: UIImage
      */
-    public func image(size:CGSize)->UIImage {
+    @objc public func image(size:CGSize)->UIImage {
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
         
         let textRect  = CGRect(x: 0, y: 0, width: size.width, height: size.height)
