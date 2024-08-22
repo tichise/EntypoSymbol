66b115be5dfe15716cc11cf6be9f461a34984532
tichise <43707+tichise@users.noreply.github.com>
2024-08-22 20:44:45 +0900

    chore: Update EntypoSymbol package manifest for iOS 15 support

--- Package.swift
+++ Package.swift
@@ -1,26 +1,19 @@
 // swift-tools-version:5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "EntypoSymbol",
-  platforms: [
-    .iOS(.v15)
-  ],
-  products: [
-    .library(
-      name: "EntypoSymbol",
-      targets: ["EntypoSymbol"]),
-  ],
-  dependencies: [
-  ],
-  targets: [
-    .target(
-        name: "EntypoSymbol",
-        dependencies: [],
-        path: "Sources"
-    ),
-    .testTarget(
-      name: "EntypoSymbolTests",
-      dependencies: ["EntypoSymbol"]),
-  ]
+    name: "EntypoSymbol",
+    platforms: [.iOS(.v12),
+                .watchOS(.v4)],
+    products: [
+        .library(name: "EntypoSymbol", targets: ["EntypoSymbol"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "EntypoSymbol",
+                resources: [
+                    .process("Resources")])
+    ],
+    swiftLanguageVersions: [.v5]
 )
