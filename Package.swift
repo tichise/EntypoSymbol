1e43b8b4a13982194007aa558693d25c39077b9e
tichise <43707+tichise@users.noreply.github.com>
2024-08-22 16:49:06 +0900

    chore: Update EntypoSymbol package manifest for iOS 15 support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
-  name: "EntypoSymbol"
+  name: "EntypoSymbol",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "EntypoSymbol",
+      targets: ["EntypoSymbol"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+        name: "EntypoSymbol",
+        dependencies: [],
+        path: "Sources"
+    ),
+    .testTarget(
+      name: "EntypoSymbolTests",
+      dependencies: ["EntypoSymbol"]),
+  ]
 )
